24145eecc4729839a560270fba5443ee1011d5ff
loppam <lolade132@gmail.com>
2023-04-29 17:20:52 +0100

    cwa

--- src/assets/shopping.jsx
+++ src/assets/shopping.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './shopping.css'
+import "./shopping.css";
 const shopping = () => {
   return (
     <div className="shopping">
@@ -34,16 +34,6 @@ const shopping = () => {
         </div>
       </div>
       <div class="marqueet">
-        <div>
-          <span>COMING SOON</span>
-          <span>COMING SOON</span>
-          <span>COMING SOON</span>
-          <span className="hidden">COMING SOON</span>
-          <span className="hidden">COMING SOON</span>
-          <span className="hidden">COMING SOON</span>
-        </div>
-      </div>
-      <div class="marquee">
         <div>
           <span>COMING SOON</span>
           <span>COMING SOON</span>
